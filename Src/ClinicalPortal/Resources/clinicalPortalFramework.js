720bcebb6a94373384a5fc1a3beabea70cfcbb7b
hiearth <hiearth@example.com>
2012-10-22 17:51:52 +0800

    add framework js file

---
+++ Src/ClinicalPortal/Resources/clinicalPortalFramework.js
@@ -0,0 +1,15 @@
+﻿
+var cp = (function () {
+
+    var cpObj = {
+        requestJson: function (url, method, successFn) {
+            $.ajax({
+                "type": "POST",
+                "url": url,
+                "contentType": "application/json; charset=utf-8",
+                "datatype": "json",
+                "success": successFn
+            });
+        }
+    };
+});
